cf5d976997e1f79d974e01c8f4b3ebd976085622
VH-tech <hariramaniv@gmail.com>
2024-06-22 09:30:49 +0530

    feat: Add PHMViT model and test script
    This commit adds a new file `PHMViT-test.py` which contains code for the PHMViT model and a test script. The PHMViT model is a variant of the Vision Transformer (ViT) model and is used for image classification tasks. The test script demonstrates the usage of both the PHMViT and ViT models by printing the model architecture and the shape of the output tensor.

---
+++ harirabani/PHMViT-test.py
@@ -0,0 +1,34 @@
+from ViT import PHMViT, ViT
+import torch
+
+print("PHMViT Model")
+
+PHMmodel = PHMViT(
+    image_size = 224,
+    patch_size = 16,
+    num_classes = 10,
+    dim = 512,
+    depth = 6,
+    heads = 8,
+    mlp_dim = 1024,
+    dropout = 0.1,
+    emb_dropout = 0.1
+)
+
+print(sum(p.numel() for p in PHMmodel.parameters()))
+
+model = ViT(
+    image_size = 224,
+    patch_size = 16,
+    num_classes = 10,
+    dim = 512,
+    depth = 6,
+    heads = 8,
+    mlp_dim = 1024,
+    dropout = 0.1,
+    emb_dropout = 0.1
+)
+
+print(sum(p.numel() for p in model.parameters()))
+
+print(PHMmodel(torch.randn(1, 3, 224, 224)).shape)
---
+++ harirabani/ViT.py
@@ -0,0 +1,246 @@
+import torch
+from torch import nn
+
+from einops import rearrange, repeat
+from einops.layers.torch import Rearrange
+from hypercomplex.layers import PHMLinear
+#PHM
+# Simple PHM_MLP Model
+class PHMLinearLayer(nn.Module):
+    def __init__(self, input_size, output_size):
+        super(PHMLinearLayer, self).__init__()
+        self.config = lambda: None
+        self.config.reduction_factor = 32 #
+        self.config.non_linearity = "gelu_new" #
+        self.config.phm_c_init = "normal" #
+        self.config.hypercomplex_division = 4 #
+        self.config.learn_phm = True #
+        self.config.hypercomplex_nonlinearity = "glorot-uniform" #
+        self.config.shared_phm_rule = False #
+        self.config.factorized_phm = False #
+        self.config.shared_W_phm = False #*
+        self.config.factorized_phm_rule = False #
+        self.config.phm_rank = 1 #
+        self.config.phm_init_range = 0.0001 #
+        self.config.kronecker_prod = False #
+
+        self.fc1 = PHMLinear(in_features=input_size,
+                                      out_features=output_size,
+                                      bias=True,
+                                      c_init=self.config.phm_c_init,
+                                      phm_dim=self.config.hypercomplex_division,
+                                      learn_phm=self.config.learn_phm,
+                                      w_init=self.config.hypercomplex_nonlinearity,
+                                      shared_phm_rule=self.config.shared_phm_rule,
+                                      factorized_phm=self.config.factorized_phm,
+                                      shared_W_phm=self.config.shared_W_phm,
+                                      factorized_phm_rule=self.config.factorized_phm_rule,
+                                      phm_rank=self.config.phm_rank,
+                                      phm_init_range=self.config.phm_init_range,
+                                      kronecker_prod=self.config.kronecker_prod)
+       
+    def forward(self, x):
+        out = self.fc1(x)
+        return out
+
+
+# helpers
+
+def pair(t):
+    return t if isinstance(t, tuple) else (t, t)
+
+# classes
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, hidden_dim, dropout = 0.):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.LayerNorm(dim),
+            nn.Linear(dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, dim),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+    
+class PHMFeedForward(nn.Module):
+    def __init__(self, dim, hidden_dim, dropout = 0.):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.LayerNorm(dim),
+            PHMLinearLayer(dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            PHMLinearLayer(hidden_dim, dim),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+class Attention(nn.Module):
+    def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0.):
+        super().__init__()
+        inner_dim = dim_head *  heads
+        project_out = not (heads == 1 and dim_head == dim)
+
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+
+        self.norm = nn.LayerNorm(dim)
+
+        self.attend = nn.Softmax(dim = -1)
+        self.dropout = nn.Dropout(dropout)
+
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, dim),
+            nn.Dropout(dropout)
+        ) if project_out else nn.Identity()
+
+    def forward(self, x):
+        x = self.norm(x)
+
+        qkv = self.to_qkv(x).chunk(3, dim = -1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.heads), qkv)
+
+        dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
+
+        attn = self.attend(dots)
+        attn = self.dropout(attn)
+
+        out = torch.matmul(attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        return self.to_out(out)
+
+class Transformer(nn.Module):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout = 0.):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                Attention(dim, heads = heads, dim_head = dim_head, dropout = dropout),
+                FeedForward(dim, mlp_dim, dropout = dropout)
+            ]))
+
+    def forward(self, x):
+        for attn, ff in self.layers:
+            x = attn(x) + x
+            x = ff(x) + x
+
+        return self.norm(x)
+
+class PHMTransformer(nn.Module):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout = 0.):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                Attention(dim, heads = heads, dim_head = dim_head, dropout = dropout),
+                PHMFeedForward(dim, mlp_dim, dropout = dropout)
+            ]))
+
+    def forward(self, x):
+        for attn, ff in self.layers:
+            x = attn(x) + x
+            x = ff(x) + x
+
+        return self.norm(x)
+
+class ViT(nn.Module):
+    def __init__(self, *, image_size, patch_size, num_classes, dim, depth, heads, mlp_dim, pool = 'cls', channels = 3, dim_head = 64, dropout = 0., emb_dropout = 0.):
+        super().__init__()
+        image_height, image_width = pair(image_size)
+        patch_height, patch_width = pair(patch_size)
+
+        assert image_height % patch_height == 0 and image_width % patch_width == 0, 'Image dimensions must be divisible by the patch size.'
+
+        num_patches = (image_height // patch_height) * (image_width // patch_width)
+        patch_dim = channels * patch_height * patch_width
+        assert pool in {'cls', 'mean'}, 'pool type must be either cls (cls token) or mean (mean pooling)'
+
+        self.to_patch_embedding = nn.Sequential(
+            Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = patch_height, p2 = patch_width),
+            nn.LayerNorm(patch_dim),
+            nn.Linear(patch_dim, dim),
+            nn.LayerNorm(dim),
+        )
+
+        self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, dim))
+        self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
+        self.dropout = nn.Dropout(emb_dropout)
+
+        self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
+
+        self.pool = pool
+        self.to_latent = nn.Identity()
+
+        self.mlp_head = nn.Linear(dim, num_classes)
+
+    def forward(self, img):
+        x = self.to_patch_embedding(img)
+        b, n, _ = x.shape
+
+        cls_tokens = repeat(self.cls_token, '1 1 d -> b 1 d', b = b)
+        x = torch.cat((cls_tokens, x), dim=1)
+        x += self.pos_embedding[:, :(n + 1)]
+        x = self.dropout(x)
+
+        x = self.transformer(x)
+
+        x = x.mean(dim = 1) if self.pool == 'mean' else x[:, 0]
+
+        x = self.to_latent(x)
+        return self.mlp_head(x)
+
+class PHMViT(nn.Module):
+    def __init__(self, *, image_size, patch_size, num_classes, dim, depth, heads, mlp_dim, pool = 'cls', channels = 3, dim_head = 64, dropout = 0., emb_dropout = 0.):
+        super().__init__()
+        image_height, image_width = pair(image_size)
+        patch_height, patch_width = pair(patch_size)
+
+        assert image_height % patch_height == 0 and image_width % patch_width == 0, 'Image dimensions must be divisible by the patch size.'
+
+        num_patches = (image_height // patch_height) * (image_width // patch_width)
+        patch_dim = channels * patch_height * patch_width
+        assert pool in {'cls', 'mean'}, 'pool type must be either cls (cls token) or mean (mean pooling)'
+
+        self.to_patch_embedding = nn.Sequential(
+            Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = patch_height, p2 = patch_width),
+            nn.LayerNorm(patch_dim),
+            nn.Linear(patch_dim, dim),
+            nn.LayerNorm(dim),
+        )
+
+        self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, dim))
+        self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
+        self.dropout = nn.Dropout(emb_dropout)
+
+        self.transformer = PHMTransformer(dim, depth, heads, dim_head, mlp_dim, dropout)
+
+        self.pool = pool
+        self.to_latent = nn.Identity()
+
+        self.mlp_head = nn.Linear(dim, num_classes)
+
+    def forward(self, img):
+        x = self.to_patch_embedding(img)
+        b, n, _ = x.shape
+
+        cls_tokens = repeat(self.cls_token, '1 1 d -> b 1 d', b = b)
+        x = torch.cat((cls_tokens, x), dim=1)
+        x += self.pos_embedding[:, :(n + 1)]
+        x = self.dropout(x)
+
+        x = self.transformer(x)
+
+        x = x.mean(dim = 1) if self.pool == 'mean' else x[:, 0]
+
+        x = self.to_latent(x)
+        return self.mlp_head(x)
